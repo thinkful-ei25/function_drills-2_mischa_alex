28094ff2bbe89ab1dd7b6ee502bbde49db463187
talexcrowell <talexcrowell@gmail.com>
2018-09-24 12:53:50 -0700

    added howManyDays

--- function_drills-2_mischa_alex.js
+++ function_drills-2_mischa_alex.js
@@ -47,3 +47,42 @@ function decode(str){
 let decoded = decode('craft block argon meter bells brown croon droop');
 console.log(decoded);
 
+function howManyDays (month, leapYear = false) {
+  try {
+    switch (month) {
+      case 'January':
+      case 'March':
+      case 'May':
+      case 'July':
+      case 'August':
+      case 'October':
+      case 'December':
+        return `${month} has 31 days.`;
+        break;
+      case 'February':
+        if (leapYear === false) {
+          return 'February has 28 days.';
+          break;
+      } else {
+          return 'Feburary has 29 days.';
+          break;
+      }
+      case 'April':
+      case 'June':
+      case 'September':
+      case 'November':
+        return `${month} has 30 days.`;
+        break;
+      default:
+        throw new Error('Please enter a valid month');
+        break;
+    }
+  }
+  catch(e) {
+    return e.message;
+  }
+}
+
+let days = howManyDays('January');
+console.log(days);
+
