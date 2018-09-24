f41690ab4c7adbe0650cc561b6a6ab5b4b6dfd5f
talexcrowell <talexcrowell@gmail.com>
2018-09-24 12:15:28 -0700

    added beyond function

--- function_drills-2_mischa_alex.js
+++ function_drills-2_mischa_alex.js
@@ -4,3 +4,18 @@ function jediName(firstName, lastName){
 
 const myJediName = jediName('Mischa','Berlin');
 console.log(myJediName);
+
+function beyond (num) {
+  if (num !== 0) {
+    if (num > 0) {
+      return 'To infinity and beyond';
+    } else {
+      return 'To negative infinity and beyond';
+    }
+  } else {
+    return 'Staying home';
+  }
+}
+
+let buzzLightyear = beyond(0);
+console.log(buzzLightyear);
